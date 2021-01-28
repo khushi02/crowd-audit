c0b351fbcdf3223ea3cab8546beeb2a7bcd58b3e
Charley Zhao <charleyjzhao@gmail.com>
2021-01-28 00:17:53 -0500

    Create basic landing page with header and footer

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.landingContainer}>
+          <div className={styles.left}>
+            <p>Crowd Audit</p>
+          </div>
+          <div className={styles.right}>
+            <p>Contact Us</p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ components/Navigation.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-lg navbar-light"
+        style={{ backgroundColor: "#d2e2d9" }}
+      >
+        <a className="navbar-brand" href="/">
+          Crowd Audit
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Hypotheses
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link " href="/">
+                Sign in/Register
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
