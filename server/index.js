8f8c15fd782725d894f80c3e886874d550cdef57
Kyungmin Kim <kyminbb@Kyungmins-MacBook-Pro.local>
2021-02-01 21:02:06 +0900

    Setup Mongoose and define basic schemas

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+// mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, err => console.error(err));
 
 app.get('/', (req, res) => res.send('<h2>Hello from the server!</h2>'));
 
---
+++ server/models/Image.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const imageSchema = new Schema({
+
+});
+
+mongoose.model('images', imageSchema);
