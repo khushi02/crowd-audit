985994f54efd00ae356cf90142416ca202d68c7e
Charley Zhao <charleyjzhao@gmail.com>
2021-03-26 15:05:43 -0400

    Rename next image button

--- pages/audit_submitted.js
+++ pages/audit_submitted.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import styles from "./audit.module.scss";
 import Button from "react-bootstrap/Button";
 
@@ -83,17 +83,11 @@ class AuditSubmitted extends Component {
                     <div className={styles.nextImage}>
                       <img src="/nextImage.png" alt="img" />
                       <div className={styles.buttonContainer}>
-<<<<<<< HEAD
-                        <a href="#" className={`btn ${styles.cardButton}`}>
-                          Audit Next Image
-                        </a>
-=======
                         <Link href="/records">
                           <div className={`btn ${styles.cardButton}`}>
-                            Audit Records
+                            Audit Next Image
                           </div>
                         </Link>
->>>>>>> a0437c0 (Link up pages)
                       </div>
                     </div>
                   </div>
