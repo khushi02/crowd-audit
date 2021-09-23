048332e6bbc2a571c05bcab5d576830efb9948d6
khushi02 <k9wadhwa@gmail.com>
2021-09-23 11:54:52 -0400

    discussion collection page

---
+++ pages/discussions.jsx
@@ -0,0 +1,5 @@
+import DiscussCollectionPageTemplate from '../components/templates/DiscussCollectionPageTemplate'
+
+const DiscussionPage = () => <DiscussCollectionPageTemplate />
+
+export default DiscussionPage
